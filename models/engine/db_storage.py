a73c9560bb779411e320be93d9b384cc47fb08e6
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-09-23 06:30:17 -0700

    feat: DBStorage - Place

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,7 +27,7 @@ class DBStorage:
             "Place": Place,
             "Review": Review
         }
-    __tables = ["State", "City", "User"]
+    __tables = ["State", "City", "User", "Place"]
 
     def __init__(self):
         """initialisation instance method"""
