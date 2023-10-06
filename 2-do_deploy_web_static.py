9a063c1ab463a4575fdb7d906c2fddd6dcde1342
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-06 02:30:03 -0700

    fix: update my server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,31 +13,29 @@ def do_deploy(archive_path):
     """
     if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    file_path = archive_path.split("/")
+    dir_ = "/".join(file_path[:-1])
+    file_name = file_path[-1].split(".")[0]
+    if put(archive_path, "/tmp/{}.tgz".format(file_name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/"
+            .format(file_name)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+    if run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+            .format(file_name, file_name)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+    if run("rm /tmp/{}.tgz".format(file_name)).failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+    if run("mv /data/web_static/releases/{}/web_static/*\
+             /data/web_static/releases/{}/"
+            .format(file_name, file_name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(file_name)).failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(file_name)).failed:
         return False
     return True
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -45,29 +45,29 @@ def do_deploy(archive_path):
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if put(archive_path, "/tmp/{}".format(file)).failed:
         return False
     if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+           format(name)).failed:
         return False
     if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+           format(name)).failed:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+           format(file, name)).failed:
         return False
-    if run("rm /tmp/{}".format(file)).failed is True:
+    if run("rm /tmp/{}".format(file)).failed:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+           "/data/web_static/releases/{}/".format(name, name)).failed:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+           format(name)).failed:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("rm -rf /data/web_static/current").failed:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+           format(name)).failed:
         return False
     return True
 
