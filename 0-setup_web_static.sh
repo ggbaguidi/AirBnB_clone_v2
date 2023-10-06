db3b35e870c903755131135a130759c416ec963a
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-06 05:57:43 -0700

    rewrite the root

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,6 +19,6 @@ cat << EOF > /data/web_static/releases/test/index.html
 EOF
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/^\tlocation \/ {.*/i \\tlocation \/hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '/^\tlocation \/ {.*/i \\tlocation \/hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 service nginx restart
