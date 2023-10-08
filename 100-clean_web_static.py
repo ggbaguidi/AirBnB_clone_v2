d2ec294ec9bdd63c486377e4d3d5006177cb5527
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-08 01:15:52 -0700

    feat: Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -16,15 +16,14 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
+    number = int(number)
+    with lcd("./versions"):
+        if number > 1:
+            local("ls -t | tail -n +{} | xargs rm -f".format(number + 1))
+        else:
+            local("ls -t | tail -n +2 | xargs rm -f")
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        if number > 1:
+            run("ls -t | tail -n +{} | xargs rm -rf".format(number + 1))
+        else:
+            run("ls -t | tail -n +2 | xargs rm -rf")
