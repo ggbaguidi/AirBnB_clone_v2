07805c394373bedcfca58d9b6651db4d39f8b377
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2023-10-20 00:10:50 +0100

    fix: state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 import os
-import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
